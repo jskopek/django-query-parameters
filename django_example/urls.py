4e8fa1e3e4f168f818f39526bdaf5611cdd7c7bd
Jean-Marc Skopek <jskopek@gmail.com>
2014-03-26 13:28:58 -0700

    Simplified demo project urls file

--- django_example/urls.py
+++ django_example/urls.py
@@ -1,13 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', TemplateView.as_view(template_name='home.html')),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
