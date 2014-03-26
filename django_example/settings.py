55d77b36b38a775c9967e46493174187ae330d6c
Jean-Marc Skopek <jskopek@gmail.com>
2014-03-26 13:28:35 -0700

    Added request context processor; required for obtaining querystring

--- django_example/settings.py
+++ django_example/settings.py
@@ -49,6 +49,17 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages"
+)
+
 ROOT_URLCONF = 'django_example.urls'
 
 WSGI_APPLICATION = 'django_example.wsgi.application'
