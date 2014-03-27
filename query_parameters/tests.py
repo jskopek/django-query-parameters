e8bfc95e2461652aec9dd1fe9452cace547ace93
Jean-Marc Skopek <jskopek@gmail.com>
2014-03-26 19:06:55 -0700

    Simplified test cases

--- query_parameters/tests.py
+++ query_parameters/tests.py
@@ -9,27 +9,17 @@ from django.http import QueryDict
 
 class SetQueryParametersTestCase(TestCase):
     def test_empty_querystring(self):
-        t = Template(
-                '{% load query_parameters %}'
-                '{% set_query_parameters prop1=val1 %}'
-            )
-        request = HttpRequest()
-        request.GET = QueryDict('')
-
-        c = RequestContext(request)
-        result = t.render(c)
+        result = self.template_generator(
+                query_string='', 
+                set_query_parameters_value='prop1=val1'
+        )
         self.assertEqual(result, 'prop1=val1')
 
     def test_new_property(self):
-        t = Template(
-                '{% load query_parameters %}'
-                '{% set_query_parameters prop1=val1 %}'
-            )
-        request = HttpRequest()
-        request.GET = QueryDict('prop2=val2')
-
-        c = RequestContext(request)
-        result = t.render(c)
+        result = self.template_generator(
+                query_string='prop2=val2', 
+                set_query_parameters_value='prop1=val1'
+        )
         self.assertEqual(result, 'prop1=val1&prop2=val2')
 
     def test_invalid_parameters(self):
@@ -37,39 +27,38 @@ class SetQueryParametersTestCase(TestCase):
         self.assertRaises(TemplateSyntaxError, Template, '{% load query_parameters %}{% set_query_parameters prop1 %}')
 
     def test_multiple_new_properties(self):
-        t = Template(
-                '{% load query_parameters %}'
-                '{% set_query_parameters prop1=val1 prop2=val2 %}'
-            )
-        request = HttpRequest()
-        request.GET = QueryDict('prop3=val3')
-
-        c = RequestContext(request)
-        result = t.render(c)
+        result = self.template_generator(
+                query_string='prop3=val3',
+                set_query_parameters_value='prop1=val1 prop2=val2'
+        )
         self.assertEqual(result, 'prop1=val1&prop2=val2&prop3=val3')
 
     def test_update_property(self):
-        t = Template(
-                '{% load query_parameters %}'
-                '{% set_query_parameters prop1=val1 %}'
-            )
-        request = HttpRequest()
-        request.GET = QueryDict('prop1=val2')
-
-        c = RequestContext(request)
-        result = t.render(c)
+        result = self.template_generator(
+                query_string='prop1=val2', 
+                set_query_parameters_value='prop1=val1'
+        )
         self.assertEqual(result, 'prop1=val1')
 
     def test_update_and_add_property(self):
+        result = self.template_generator(
+                query_string='prop1=val1&prop2=val2', 
+                set_query_parameters_value='prop1=val1_modified prop3=val3'
+        )
+        self.assertEqual(result, 'prop1=val1_modified&prop2=val2&prop3=val3')
+
+    def template_generator(self, query_string, set_query_parameters_value):
+        """
+        Helper method to simplify generating a template with a mock `query_string` and `set_query_parameters_value`
+        """
         t = Template(
                 '{% load query_parameters %}'
-                '{% set_query_parameters prop1=val1_modified prop3=val3 %}'
+                '{% set_query_parameters ' + set_query_parameters_value + ' %}'
             )
         request = HttpRequest()
-        request.GET = QueryDict('prop1=val1&prop2=val2')
+        request.GET = QueryDict(query_string)
 
         c = RequestContext(request)
         result = t.render(c)
-        self.assertEqual(result, 'prop1=val1_modified&prop2=val2&prop3=val3')
-
+        return result
 
