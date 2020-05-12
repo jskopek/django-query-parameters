22cc516b8e79bf463696c8b51b3f643b4b639f0f
Jean-Marc Skopek <jskopek@gmail.com>
2020-05-11 18:52:06 -0700

    Testing set_query_parameters templatetag on simple variable and dict variable

--- django_example/views.py
+++ django_example/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class TestView(TemplateView):
     template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['simple_variable'] = 'hello world'
+        context['dict_variable'] = {'hello': 'world'}
+        return context
